b5046707456ea0c61eb08120897d809d1402987f
Wheberton Fialho <whebertonfialho@gmail.com>
2019-02-17 19:53:03 -0400

    Alterações de Nome

--- CaladoraMotor/CalculadoraMotor.py
+++ CaladoraMotor/CalculadoraMotor.py
@@ -3,18 +3,29 @@
 
 pi = 3.14159
 
-def cilindrada(dm_pistao, dm_curso, cilindros):
+def CalculaCilindrada(dm_pistao, dm_curso, cilindros):
 	multiplicacao = pi * (dm_pistao * dm_pistao) * dm_curso
-	
+	cilindrada = 0
+
 	#MONOCILINDRO OU MULTICILINDRO
 	if cilindros == 1:
-		print "Resultado e {:.2f} cm3".format((multiplicacao / 1000) / 4)
+		cilindradas = (multiplicacao / 1000) / 4
 	else:
 		resultado = cilindros == 4 and multiplicacao or (multiplicacao / 4) * cilindros
-		print "Resultado e {:.2f} cm3".format(resultado / 1000)
+		cilindradas = round(resultado / 1000, 2)
 	
+	return cilindradas
+
+def CalculaTaxaMotor():
+	pass
+
+def CalculaVolumeMotor():
+	pass
+
 if __name__ == '__main__':
 	dm_pistao = input("Digite o Diametro do Pistao(mm): ")
 	dm_curso = input("Curso Virabrequin(mm): ")
 	cilindros = input("Digite quantos cilindros: ")
-	cilindrada(dm_pistao, dm_curso, cilindros)
+	cilindradas = CalculaCilindrada(dm_pistao, dm_curso, cilindros)
+
+	print "Cilindradas: {0}cc".format(cilindradas)
